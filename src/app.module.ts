06f074fd9e5d3f3007dd0ee1ab4b16012e82def8
Phong Phi Gia <tpw284@gmail.com>
2021-12-18 10:28:43 +0700

    gen

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { SurveyModule } from './survey/survey.module';
+import { UsersModule } from './users/users.module';
+import { SurveysModule } from './surveys/surveys.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    SurveyModule,
+    UsersModule,
+    MikroOrmModule.forRoot({
+      entities: ['./dist/entities'],
+      entitiesTs: ['./src/entities'],
+      dbName: 'Quiz',
+      type: 'postgresql',
+    }),
+    SurveysModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
